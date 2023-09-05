8f7f85d781e7e6038839e2f629661d065100c4f4
Ismael Santos <isant028@fiu.edu>
2023-09-04 22:31:52 -0400

    Updated App.js to fix error in code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import TerminalGame from './components/TerminalGame/TerminalGame';
 
-
+//ResizeEventObserver loop can cause a notification to pop up, but error seems benign and built-in to avoid infinite loops. Stack overflow link has more info on it
+//https://stackoverflow.com/questions/49384120/resizeobserver-loop-limit-exceeded/50387233#50387233
 function App() {
  
   return (
